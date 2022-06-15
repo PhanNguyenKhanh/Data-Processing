6f13f9edecdd5e115ef64e9f71d334e681da096e
nguyên khánh phan <khanhkn987@gmail.com>
2022-06-15 19:39:30 +0700

    Add Open/Save Dialog

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -30,15 +30,14 @@ namespace WindowsFormsApp2
         private void InitializeComponent()
         {
             this.btnGenerate = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.lState = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.InputFile = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(13, 13);
+            this.btnGenerate.Location = new System.Drawing.Point(384, 12);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(75, 23);
             this.btnGenerate.TabIndex = 0;
@@ -46,45 +45,46 @@ namespace WindowsFormsApp2
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
-            // label1
+            // lState
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lState.AutoSize = true;
+            this.lState.Location = new System.Drawing.Point(465, 17);
+            this.lState.Name = "lState";
+            this.lState.Size = new System.Drawing.Size(48, 13);
+            this.lState.TabIndex = 1;
+            this.lState.Text = "Success";
             // 
-            // dataGridView1
+            // btnBrowse
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 59);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 2;
+            this.btnBrowse.Location = new System.Drawing.Point(303, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
-            // button1
+            // InputFile
             // 
-            this.button1.Location = new System.Drawing.Point(95, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.InputFile.Location = new System.Drawing.Point(12, 14);
+            this.InputFile.Name = "InputFile";
+            this.InputFile.Size = new System.Drawing.Size(285, 20);
+            this.InputFile.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(556, 57);
+            this.Controls.Add(this.InputFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lState);
             this.Controls.Add(this.btnGenerate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Data Processing";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,9 +93,9 @@ namespace WindowsFormsApp2
         #endregion
 
         private System.Windows.Forms.Button btnGenerate;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lState;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox InputFile;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -43,137 +43,127 @@ namespace WindowsFormsApp2
         }
         private void btnGenerate_Click(object sender, EventArgs e)
         {
-            label1.Text = "Processing...";
-
-            int j = 2;
-            string tIn = "";
-            string tOut = "";
-            Boolean flagIn = false;
-
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(@"D:\Data_processing\BGSV_TimeLog_Input.xlsx");
-            Excel.Worksheet xlWorkSheet = xlWorkBook.Sheets[1];
-            Excel.Range xlRange = xlWorkSheet.UsedRange;
-
-            Excel.Workbook xlWorkBook1;
-            Excel.Worksheet xlWorkSheet1;
-            object missValue = System.Reflection.Missing.Value;
-
-            xlWorkBook1 = xlApp.Workbooks.Add(missValue);
-            xlWorkSheet1 = (Excel.Worksheet)xlWorkBook1.Worksheets.get_Item(1);
-
-            int rowCount = xlRange.Rows.Count;
-
-            xlWorkSheet1.Cells[1, 1] = "Staff's Name";
-            xlWorkSheet1.Cells[1, 2] = "Date";
-            xlWorkSheet1.Cells[1, 3] = "Day";
-            xlWorkSheet1.Cells[1, 4] = "Time in";
-            xlWorkSheet1.Cells[1, 5] = "Time out";
-            xlWorkSheet1.Cells[1, 6] = "Total Working Time";
-
-            /*xlWorkSheet1.Cells[2, 1] = xlRange.Cells[2, 4].Value.ToString();
-            xlWorkSheet1.Cells[2, 2] = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 0);
-            xlWorkSheet1.Cells[2, 3] = DayOfWeek(splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 0));
-            xlWorkSheet1.Cells[2, 4] = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 1);
-            tIn = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 1);
-
-            for (int i = 3; i <= rowCount; i++)
+            lState.Text = "Processing...";
+            try
             {
-                if(splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0) != splitString(xlRange.Cells[(i - 1), 1].Value.ToString(), ' ', 0) || xlRange.Cells[i, 4].Value.ToString() != xlRange.Cells[(i - 1), 4].Value.ToString())
+                int j = 2;
+                string tIn = "";
+                string tOut = "";
+                Boolean flagIn = false;
+
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(@"" + InputFile.Text);
+                Excel.Worksheet xlWorkSheet = xlWorkBook.Sheets[1];
+                Excel.Range xlRange = xlWorkSheet.UsedRange;
+
+                Excel.Workbook xlWorkBook1;
+                Excel.Worksheet xlWorkSheet1;
+                object missValue = System.Reflection.Missing.Value;
+
+                xlWorkBook1 = xlApp.Workbooks.Add(missValue);
+                xlWorkSheet1 = (Excel.Worksheet)xlWorkBook1.Worksheets.get_Item(1);
+
+                int rowCount = xlRange.Rows.Count;
+
+                xlWorkSheet1.Cells[1, 1] = "Staff's Name";
+                xlWorkSheet1.Cells[1, 2] = "Date";
+                xlWorkSheet1.Cells[1, 3] = "Day";
+                xlWorkSheet1.Cells[1, 4] = "Time in";
+                xlWorkSheet1.Cells[1, 5] = "Time out";
+                xlWorkSheet1.Cells[1, 6] = "Total Working Time";
+
+                xlWorkSheet1.Cells[2, 1] = xlRange.Cells[2, 4].Value.ToString();
+                xlWorkSheet1.Cells[2, 2] = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 0);
+                xlWorkSheet1.Cells[2, 3] = DayOfWeek(splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 0));
+
+                if (xlRange.Cells[2, 2].Value.ToString() == "entry reader 1")
                 {
-                    xlWorkSheet1.Cells[j, 5] = splitString(xlRange.Cells[(i - 1), 1].Value.ToString(), ' ', 1);
-                    tOut = splitString(xlRange.Cells[(i - 1), 1].Value.ToString(), ' ', 1);
+                    xlWorkSheet1.Cells[2, 4] = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 1);
+                    tIn = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 1);
+                    flagIn = true;
+                }
+
+                for (int i = 3; i <= rowCount; i++)
+                {
+                    if (splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0) != splitString(xlRange.Cells[(i - 1), 1].Value.ToString(), ' ', 0) || xlRange.Cells[i, 4].Value.ToString() != xlRange.Cells[(i - 1), 4].Value.ToString())
+                    {
+                        if (tIn != null && tOut != null)
+                        {
+                            xlWorkSheet1.Cells[j, 6] = TotalWorkingTime(tIn, tOut);
+                        }
+                        j++;
+                        xlWorkSheet1.Cells[j, 1] = xlRange.Cells[i, 4].Value.ToString();
+                        xlWorkSheet1.Cells[j, 2] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0);
+                        xlWorkSheet1.Cells[j, 3] = DayOfWeek(splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0));
+                        flagIn = false;
+                    }
+                    if (flagIn == false)
+                    {
+                        if (xlRange.Cells[i, 2].Value.ToString() == "entry reader 1")
+                        {
+                            xlWorkSheet1.Cells[j, 4] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
+                            tIn = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
+                            flagIn = true;
+                        }
+                    }
+                    xlWorkSheet1.Cells[j, 5] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
+                    tOut = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
+                }
+
+                if (tIn != null && tOut != null)
+                {
                     xlWorkSheet1.Cells[j, 6] = TotalWorkingTime(tIn, tOut);
-                    xlWorkSheet1.Cells[(j + 1), 1] = xlRange.Cells[i, 4].Value.ToString();
-                    xlWorkSheet1.Cells[(j + 1), 2] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0);
-                    xlWorkSheet1.Cells[(j + 1), 3] = DayOfWeek(splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0));
-                    xlWorkSheet1.Cells[(j + 1), 4] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
-                    tIn = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
-                    j++;
                 }
-            }
 
-            xlWorkSheet1.Cells[j, 5] = splitString(xlRange.Cells[rowCount, 1].Value.ToString(), ' ', 1);
-            tOut = splitString(xlRange.Cells[rowCount, 1].Value.ToString(), ' ', 1);
-            xlWorkSheet1.Cells[j, 6] = TotalWorkingTime(tIn, tOut);*/
-            xlWorkSheet1.Cells[2, 1] = xlRange.Cells[2, 4].Value.ToString();
-            xlWorkSheet1.Cells[2, 2] = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 0);
-            xlWorkSheet1.Cells[2, 3] = DayOfWeek(splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 0));
+                //Set Column Width
+                xlWorkSheet1.Columns.AutoFit();
+                //xlWorkSheet1.Columns[1].ColumnWidth = 30;
 
-            if (xlRange.Cells[2, 2].Value.ToString() == "entry reader 1")
-            {
-                xlWorkSheet1.Cells[2, 4] = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 1);
-                tIn = splitString(xlRange.Cells[2, 1].Value.ToString(), ' ', 1);
-                flagIn = true;
-            }
+                //Format Cells
+                xlWorkSheet1.get_Range("A1", "F1").Interior.Color = Excel.XlRgbColor.rgbYellow;
+                xlWorkSheet1.get_Range("A1", "F1").Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
 
-            for(int i = 3; i <= rowCount; i++)
-            {
-                if (splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0) != splitString(xlRange.Cells[(i - 1), 1].Value.ToString(), ' ', 0) || xlRange.Cells[i, 4].Value.ToString() != xlRange.Cells[(i - 1), 4].Value.ToString())
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                DialogResult result = saveFileDialog.ShowDialog();
+                if (result == DialogResult.OK)
                 {
-                    if (tIn != null && tOut != null)
-                    {
-                        xlWorkSheet1.Cells[j, 6] = TotalWorkingTime(tIn, tOut);
-                    }
-                    j++;
-                    xlWorkSheet1.Cells[j, 1] = xlRange.Cells[i, 4].Value.ToString();
-                    xlWorkSheet1.Cells[j, 2] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0);
-                    xlWorkSheet1.Cells[j, 3] = DayOfWeek(splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 0));
-                    flagIn = false;
+                    xlWorkBook1.SaveAs(@"" + saveFileDialog.FileName, Excel.XlFileFormat.xlWorkbookNormal, missValue, missValue, missValue, missValue, Excel.XlSaveAsAccessMode.xlExclusive, missValue, missValue, missValue, missValue, missValue);
                 }
-                if (flagIn == false)
-                {
-                    if (xlRange.Cells[i, 2].Value.ToString() == "entry reader 1")
-                    {
-                        xlWorkSheet1.Cells[j, 4] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
-                        tIn = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
-                        flagIn = true;
-                    }
-                }
-                xlWorkSheet1.Cells[j, 5] = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
-                tOut = splitString(xlRange.Cells[i, 1].Value.ToString(), ' ', 1);
-            }
 
-            if (tIn != null && tOut != null)
+                xlWorkBook1.Close(false, missValue, missValue);
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                Marshal.ReleaseComObject(xlWorkSheet1);
+                Marshal.ReleaseComObject(xlWorkBook1);
+
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorkSheet);
+
+                xlWorkBook.Close();
+                Marshal.ReleaseComObject(xlWorkBook);
+
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+
+                lState.Text = "Success";
+            }
+            catch(Exception ex)
             {
-                xlWorkSheet1.Cells[j, 6] = TotalWorkingTime(tIn, tOut);
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lState.Text = "Error";
             }
-
-            //Set Column Width
-            xlWorkSheet1.Columns.AutoFit();
-            //xlWorkSheet1.Columns[1].ColumnWidth = 30;
-
-            //Format Cells
-            xlWorkSheet1.get_Range("A1", "F1").Interior.Color = Excel.XlRgbColor.rgbYellow;
-            xlWorkSheet1.get_Range("A1", "F1").Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
-
-            xlWorkBook1.SaveAs("d:\\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookNormal, missValue, missValue, missValue, missValue, Excel.XlSaveAsAccessMode.xlExclusive, missValue, missValue, missValue, missValue, missValue);
-            xlWorkBook1.Close(true, missValue, missValue);
-
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-            Marshal.ReleaseComObject(xlWorkSheet1);
-            Marshal.ReleaseComObject(xlWorkBook1);
-
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorkSheet);
-
-            xlWorkBook.Close();
-            Marshal.ReleaseComObject(xlWorkBook);
-
-            xlApp.Quit();
-            Marshal.ReleaseComObject(xlApp);
-
-            label1.Text = "Success";
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnBrowse_Click(object sender, EventArgs e)
         {
-            string timeIn = "07:30:30";
-            string timeOut = "09:20:30";
-            TimeSpan timeSpan = DateTime.Parse(timeOut) - DateTime.Parse(timeIn);
-            label1.Text = (timeSpan.Hours + (float)timeSpan.Minutes / 60).ToString("0.0");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            DialogResult result = openFileDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                InputFile.Text = openFileDialog.FileName;
+            }
         }
     }
 }
