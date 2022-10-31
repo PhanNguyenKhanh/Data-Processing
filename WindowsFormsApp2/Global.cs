016c374210f0b42af43d4d22195e168425462272
nguyên khánh phan <khanhkn987@gmail.com>
2022-10-31 14:08:19 +0700

    add class Global

---
+++ WindowsFormsApp2/Global.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsFormsApp2
+{
+    class Global
+    {
+        public static string[] listSheet;
+        public static int sheetIndex;
+        public static int colDate;
+        public static int colName;
+    }
+}
